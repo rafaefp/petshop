f2745c9c5a52138d47f8c1e08d059faae738efcc
rafaefp <rfpimentel@outlook.com>
2021-03-25 11:02:43 -0300

    adicionado template string

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ let pets = [{
 
 const listarPets = () => {
     for (var i = 0; i < pets.length; i++) {
-        console.log(pets[i].nome);
+        // console.log(pets[i].nome);
+        console.log(`O nome do pet é ${pets[i].nome}`);
     }
 }
 
