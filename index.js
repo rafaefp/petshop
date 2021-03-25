d2acd88d8703577237591c0a4b3009491a52966d
rafaefp <rfpimentel@outlook.com>
2021-03-25 14:25:01 -0300

    adicionadas funções darBanho, tosarPet e apararUnhas

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const moment = require("moment");
+
 const nomePetshop = "PETSHOP AVANADE";
 let pets = [{
     nome: 'Mika',
@@ -18,7 +20,7 @@ let pets = [{
     tutor: 'Tiago',
     contato: '(81) 98855-4565',
     vacinado: true,
-    services: ['banho', 'tosa']
+    services: ['tosa']
 }, {
     nome: 'Veneno',
     tipo: 'cachorro',
@@ -76,8 +78,41 @@ const adicionarCliente = (nome, tipo, idade, raca, peso, tutor, contato, vacinad
     console.log(pets);
 }
 
+const darBanhoPet = (petty) => {
+    for (let pet of pets) {
+        if (pet.nome === petty) {
+            pet.services.push('banho');
+            console.log(`${pet.nome} está de banho tomado!`);
+            console.log(`Serviço realizado na data: ${moment().locale('pt').format('dddd, hA')}`);            
+        }
+    }
+}
+const tosarPet = (petty) => {
+    for (let pet of pets) {
+        if (pet.nome === petty) {
+            pet.services.push('tosa');
+            console.log(`${pet.nome} está com cabelinho na régua!`);
+            console.log(`Serviço realizado na data: ${moment().locale('pt').format('dddd, hA')}`);
+        }
+    }
+}
+const apararUnhasPet = (petty) => {
+    for (let pet of pets) {
+        if (pet.nome === petty) {
+            pet.services.push('corte de unhas');
+            console.log(`${pet.nome} está com as unhas aparadas!`);
+            console.log(`Serviço realizado na data: ${moment().locale('pt').format('dddd, hA')}`);
+        }
+    }
+}
+
 
 // listarPets();
 // vacinarPets('Mika');
 // campanhaVacina();
-adicionarCliente('Celso', 'cachorro', 7, 'vira', 10, 'rafael', '(81) 58654-7987', true);
+
+// adicionarCliente('Celso', 'cachorro', 7, 'vira', 10, 'rafael', '(81) 58654-7987', true);
+darBanhoPet('Bela');
+tosarPet('Mika');
+apararUnhasPet('Veneno');
+
